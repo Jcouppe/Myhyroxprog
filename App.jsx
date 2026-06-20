2434350a710b3b559dc5bd237aa51b8a60afcc4f
Jcouppe <jeremy.couppe@gmail.com>
2026-06-20 22:32:20 +0200

    Fix syntax error in CSS selectors

--- App.jsx
+++ App.jsx
@@ -1,9 +1,26 @@
-html, body, #root {
-  margin: 0;
-  padding: 0;
-  min-height: 100%;
-  height: 100%;
-}
-body {
-  background: #ece9e0;
-}
+22:22:02.103
+ 
+[vite:esbuild] La transformation a échoué avec 1 erreur :
+22:22:02.103
+ 
+/vercel/path0/App.jsx:1:12: ERREUR : « #root » inattendu
+22:22:02.103
+ 
+fichier : /vercel/path0/App.jsx:1:12
+22:22:02.103
+ 
+22:22:02.104
+ 
+"#root" inattendu
+22:22:02.104
+ 
+1 | html, corps, #racine {
+22:22:02.104
+ 
+   | ^
+22:22:02.105
+ 
+2 | marge : 0 ;
+22:22:02.105
+ 
+3 | marge intérieure : 0 ;
